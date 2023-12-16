34210a6871107ddbdfea98f2182b3c6d8e7e5da5
zakki <zakkimuzakki050@gmail.com>
2023-12-16 12:14:05 +0700

    complete explore button

--- src/components/button/ExploreButton.jsx
+++ src/components/button/ExploreButton.jsx
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom';
 import sayapAtas from '../../assets/main-button/SayapAtas.svg';
 import sayapBawah from '../../assets/main-button/SayapBawah.svg';
-import './ExploreButton.css'; // File CSS terpisah untuk mengatur tata letak
+import './ExploreButton.css'; 
 
 function ExploreButton() {
   return (
     <div className="explore-button-container">
-      <img className="sayap-atas" src={sayapAtas} alt="Sayap Atas"/>
-      <img className="sayap-bawah" src={sayapBawah} alt="Sayap Bawah"/>
+      <img className="sayap-atas-kiri" src={sayapAtas} alt="Sayap Atas"/>
+      <img className="sayap-bawah-kiri" src={sayapBawah} alt="Sayap Bawah"/>
       <Link
         to="/tujuan-tautan"
         className="button-explore-link"
       >
         MULAI EKSPLORASI
       </Link>
-      <img className="sayap-atas" src={sayapAtas} alt="Sayap Atas"/>
-      <img className="sayap-bawah" src={sayapBawah} alt="Sayap Bawah"/>
+      <img className="sayap-atas-kanan" src={sayapAtas} alt="Sayap Atas"/>
+      <img className="sayap-bawah-kanan" src={sayapBawah} alt="Sayap Bawah"/>
     </div>
   );
 }
