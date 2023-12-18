0ef1c1d55e69f827b132b314f4e262cc3c4bafc7
ernuyoga <ernuyoganirwana@gmail.com>
2023-12-18 12:10:22 +0700

    data makanan

---
+++ src/data/makanan.js
@@ -0,0 +1,62 @@
+export const listMakanan = [
+    {
+        namaMakanan: "Mie Aceh",
+        daerah: "Aceh",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Pempek Palembang",
+        daerah: "Palembang",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Rendang",
+        daerah: "Padang",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Soto Lamongan",
+        daerah: "Lamongan",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Nasi Goreng",
+        daerah: "Indonesia",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Sate Madura",
+        daerah: "Madura",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Gudeg Jogja",
+        daerah: "Yogyakarta",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Coto Makassar",
+        daerah: "Makassar",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Kerak Telor",
+        daerah: "Jakarta",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    },
+    {
+        namaMakanan: "Bakso Malang",
+        daerah: "Malang",
+        foto: "[insert here]",
+        deskripsi: "[insert here]"
+    }
+]
