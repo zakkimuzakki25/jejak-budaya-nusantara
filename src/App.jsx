b9d2a34730d84c6421c8752447486c41b005b06e
ernuyoga <ernuyoganirwana@gmail.com>
2023-12-18 12:46:29 +0700

    content card temp

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import ExplorationPage from "./pages/Budaya/ExplorationPage";
+import MakananPage from "./pages/Budaya/MakananPage";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/exploration" element={<ExplorationPage />} />
+          <Route path="/makanan" element={<MakananPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/pages/Budaya/MakananPage.jsx
@@ -0,0 +1,11 @@
+import ContentCard from "../../components/cards/content/ContentCard"
+
+const MakananPage = () => {
+  return (
+    <div>
+        <ContentCard />
+    </div>
+  )
+}
+
+export default MakananPage
