bf0f17363c4970998daa7e9bfd443e50dd2bbcf2
Rino <rinooktavianridwan@gmail.com>
2023-12-19 18:55:23 +0700

    upacaradata

---
+++ src/data/upacara.jsx
@@ -0,0 +1,53 @@
+import Ngaben from "../assets/image/Ngaben.svg";
+import RambuSolo from "../assets/image/Rambu-Solo.svg";
+import AruhBaharin from "../assets/image/Aruh-Baharin.svg";
+import Tabuik from "../assets/image/Tabuik.svg";
+import Peusijuek from "../assets/image/Peusijuek.svg";
+
+export const listUpacara = [
+  {
+    namaUpacara: "Ngaben",
+    daerah: "Bali",
+    foto: Ngaben,
+    deskripsi:
+      "Upacara kremasi atau pembakaran jenazah di Bali yang dipercaya sebagai ritual untuk menyempurnakan jenazah kembali ke Sang Pencipta. Terbagi menjadi tiga jenis, yaitu Ngaben sawa Wedana, Ngaben Asti Wedana, dan Swasta.",
+    sumber:
+      "https://datulomboktour.com/wp-content/uploads/2020/02/upacara-ngaben1.jpg",
+  },
+  {
+    namaUpacara: "Rambu Solo",
+    daerah: "Toraja",
+    foto: RambuSolo,
+    deskripsi:
+      "Upacara mirip dengan Ngaben di Bali, dimaksudkan untuk mengantarkan arwah orang yang sudah meninggal menuju alam roh dan berkumpul bersama para leluhur. Orang yang meninggal namun belum menjalani upacara Rambu Solo dianggap sebagai orang sakit dan belum dimakamkan.",
+    sumber:
+      "https://asset.kompas.com/crops/ZhpxYgumlkVA-nDeqZ4TacMG3jE=/1x0:1124x749/750x500/data/photo/2021/08/25/612624cecf70a.jpg",
+  },
+  {
+    namaUpacara: "Aruh Baharin",
+    daerah: "Kalimantan",
+    foto: AruhBaharin,
+    deskripsi:
+      "Upacara adat sebagai bentuk ungkapan rasa syukur dan terima kasih kepada Tuhan Yang Maha Kuasa. Dilakukan sebagai penghormatan kepada leluhur yang diyakini melindungi mereka dari malapetaka.",
+    sumber:
+      "https://www.infopublik.id/assets/upload/headline//Aruh_Baharin,_Ritual_Adat_Dayak_Meratus_Halong.jpeg",
+  },
+  {
+    namaUpacara: "Tabuik",
+    daerah: "Sumatera Barat",
+    foto: Tabuik,
+    deskripsi:
+      "Upacara Tabuik merupakan tradisi masyarakat Minangkabau di pantai barat Sumatera Barat, diselenggarakan secara turun temurun. Digelar pada hari Asura yang jatuh setiap tanggal 10 Muharram dalam kalender islam. Peringatan meninggalnya cucu Nabi Muhammad SAW yang bernama Husein.",
+    sumber:
+      "https://assets.kompasiana.com/items/album/2021/07/19/tabuik-1-60f512121525104eb56dca02.jpeg?t=o&v=740&x=416",
+  },
+  {
+    namaUpacara: "Peusijuek",
+    daerah: "Aceh",
+    foto: Peusijuek,
+    deskripsi:
+      "Upacara sebagai ucapan rasa syukur terhadap rezeki yang diberikan Tuhan pada rakyat Aceh. Digunakan pada acara pernikahan, rumah baru, naik haji, hingga kelahiran. Juga dilakukan saat seseorang mendapat kebahagiaan, rahmat, atau terlepas dari suatu musibah.",
+    sumber:
+      "https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/24/2022/03/Peusijuek-Foto-Article-2.2-Filosofi-Tradisi-Peusijuek.jpg?width=450&quality=90",
+  },
+];
