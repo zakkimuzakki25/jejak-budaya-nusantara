4b4ce06e4817a408d722e44881abb3cdf7ed0394
ernuyoga <ernuyoganirwana@gmail.com>
2023-12-19 18:44:39 +0700

    pages fixing

---
+++ src/pages/Budaya/SenjataPage.jsx
@@ -0,0 +1,31 @@
+import ContentCard from "../../components/cards/content/ContentCard";
+import { listSenjata } from "../../data/senjata.jsx";
+
+const SenjataPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundColor: "#f1f5f9",
+        paddingBottom: "30px",
+      }}
+    >
+      {listSenjata.map((senjata, index) => (
+        <ContentCard
+          jenis="Senjata khas"
+          nama={senjata.namaSenjata}
+          daerah={senjata.daerah}
+          foto={senjata.foto}
+          deskripsi={senjata.deskripsi}
+          sumber={senjata.sumber}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SenjataPage;
--- src/pages/Budaya/UpacaraPage.jsx
+++ src/pages/Budaya/UpacaraPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import ContentCard from "../../components/cards/content/ContentCard";
+import { listUpacara } from "../../data/upacara.jsx";
 
 const UpacaraPage = () => {
   return (
-    <div>UpacaraPage</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundColor: "#f1f5f9",
+        paddingBottom: "30px",
+      }}
+    >
+      {listUpacara.map((upacara, index) => (
+        <ContentCard
+          jenis="Makanan khas"
+          nama={upacara.namaUpacara}
+          daerah={upacara.daerah}
+          foto={upacara.foto}
+          deskripsi={upacara.deskripsi}
+          sumber={upacara.sumber}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default UpacaraPage
+export default UpacaraPage;
