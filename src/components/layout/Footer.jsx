046cbb2e161f6d2d715f07ce73e07246ba76d5c2
Rino <rinooktavianridwan@gmail.com>
2023-12-19 16:44:54 +0700

    footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,47 @@
+import React from "react";
 import logo from "../../assets/logo/LogoWhite.svg";
-import "./Footer.css"
+import "./Footer.css";
+import { listNavbar } from "../../data/NavbarPopUpList";
+import { Link } from "react-router-dom";
 
 function Footer() {
+  const firstSection = listNavbar.slice(0, 5);
+  const secondSection = listNavbar.slice(5);
+
   return (
     <div className="footer-container">
-      <img className="footer-logo" src={logo} alt="logo" />
-      <h1 className="footer-copyright"> © Jejak Budaya Nusantara 2023. All rights reserved </h1>
+      <div className="footer-section">
+        <div className="footer-left">
+          <img className="footer-logo" src={logo} alt="logo" />
+          <Link >Tentang Kami</Link>
+        </div>
+        <div className="footer-right">
+          <div className="footer-navigator">
+            {firstSection.map((item, index) => (
+              <div key={index} className="slider-item">
+                <button href={item.path} className="slider-link">
+                  {item.nama}
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="footer-navigator">
+            {secondSection.map((item, index) => (
+              <div key={index} className="slider-item">
+                <button href={item.path} className="slider-link">
+                  {item.nama}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <h1 className="footer-copyright">
+        © Jejak Budaya Nusantara 2023. All rights reserved
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Exploration/ExplorationPage.jsx
+++ src/pages/Exploration/ExplorationPage.jsx
@@ -4,56 +4,88 @@ import VectorArrow from "../../assets/support/VectorArrow.svg";
 import "./ExplorationPage.css";
 
 const ExplorationPage = () => {
-    return (
-        <div className="exploration-container">
-            <div className="nusantara-container">
-                {explorationList.map((exploration, index) => (
-                    <a href={"#" + exploration.id} className="card-nusantara-letter" key={index}>
-                        {exploration.letter}
-                    </a>
-                ))}
-            </div>
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop - 150,
+        behavior: "smooth",
+      });
+    }
+  };
+  return (
+    <div className="exploration-container">
+      <div className="nusantara-container">
+        {explorationList.map((exploration, index) => (
+          <div
+            key={index}
+            className="card-nusantara-letter"
+            onClick={() => scrollToSection(exploration.id)}
+          >
+            {exploration.letter}
+          </div>
+        ))}
+      </div>
 
-            {explorationList.map((exploration, index) => (
-                <div className="card-nusantara" id={exploration.id} key={index}>
-                    {exploration.position === "left" ? (
-                        <>
-                            {/* card maskot */}
-                            <exploration.card />
+      {explorationList.map((exploration, index) => (
+        <div className="card-nusantara" id={exploration.id} key={index}>
+          {exploration.position === "left" ? (
+            <>
+              {/* card maskot */}
+              <exploration.card />
 
-                            {/* content */}
-                            <div className="content-nusantara">
-                                <div className="title">Lorem ipsum dolor sit amet, consectetur</div>
-                                <div className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut mi augue. Etiam dapibus maximus dui sit amet egestas. Vestibulum condimentum orci sed sem egestas molestie. Sed tristique dui eros, in lacinia tellus pulvinar in. Maecenas suscipit ut ligula at pretium.</div>
-                                <Link to="/detail" className="link-path">
-                                    <p>lihat selengkapnya</p>
-                                    <img src={VectorArrow} alt="Vector Arrow" />
-                                </Link>
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            {/* content */}
-                            <div className="content-nusantara" style={{
-                                justifySelf: "end",
-                                textAlign: "right",
-                            }}>
-                                <div className="title">Lorem ipsum dolor sit amet, consectetur</div>
-                                <div className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut mi augue. Etiam dapibus maximus dui sit amet egestas. Vestibulum condimentum orci sed sem egestas molestie. Sed tristique dui eros, in lacinia tellus pulvinar in. Maecenas suscipit ut ligula at pretium.</div>
-                                <Link to={exploration.path} className="link-path">
-                                    <p>lihat selengkapnya</p>
-                                    <img src={VectorArrow} alt="Vector Arrow" />
-                                </Link>
-                            </div>
-
-                            {/* card maskot */}
-                            <exploration.card />
-                        </>
-                    )}
+              {/* content */}
+              <div className="content-nusantara">
+                <div className="title">
+                  Lorem ipsum dolor sit amet, consectetur
                 </div>
-            ))}
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer ut mi augue. Etiam dapibus maximus dui sit amet
+                  egestas. Vestibulum condimentum orci sed sem egestas molestie.
+                  Sed tristique dui eros, in lacinia tellus pulvinar in.
+                  Maecenas suscipit ut ligula at pretium.
+                </div>
+                <Link to="/detail" className="link-path">
+                  <p>lihat selengkapnya</p>
+                  <img src={VectorArrow} alt="Vector Arrow" />
+                </Link>
+              </div>
+            </>
+          ) : (
+            <>
+              {/* content */}
+              <div
+                className="content-nusantara"
+                style={{
+                  justifySelf: "end",
+                  textAlign: "right",
+                }}
+              >
+                <div className="title">
+                  Lorem ipsum dolor sit amet, consectetur
+                </div>
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer ut mi augue. Etiam dapibus maximus dui sit amet
+                  egestas. Vestibulum condimentum orci sed sem egestas molestie.
+                  Sed tristique dui eros, in lacinia tellus pulvinar in.
+                  Maecenas suscipit ut ligula at pretium.
+                </div>
+                <Link to={exploration.path} className="link-path">
+                  <p>lihat selengkapnya</p>
+                  <img src={VectorArrow} alt="Vector Arrow" />
+                </Link>
+              </div>
+
+              {/* card maskot */}
+              <exploration.card />
+            </>
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ExplorationPage;
