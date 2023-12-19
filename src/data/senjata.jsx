9cbc8b37c51869dc6267e121d6d0409a71c339bf
Rino <rinooktavianridwan@gmail.com>
2023-12-19 19:15:44 +0700

    dataSenjata

---
+++ src/data/senjata.jsx
@@ -0,0 +1,53 @@
+import PedangJenawi from "../assets/image/Pedang-Jenawi.svg";
+import GolokCiomas from "../assets/image/Golok-Ciomas.svg";
+import TombakTrisula from "../assets/image/Tombak-Trisula.svg";
+import PisoGajaDompak from "../assets/image/Piso-Gaja-Dompak.svg";
+import KerisBengkulu from "../assets/image/Keris-Bengkulu.svg";
+
+export const listSenjata = [
+  {
+    namaSenjata: "Pedang Jenawi",
+    asal: "Riau",
+    foto: PedangJenawi,
+    deskripsi:
+      "Pedang Jenawi, senjata tradisional Riau, mirip dengan pedang moor dari Arab. Memiliki bilah bermata satu sepanjang satu meter dengan ujung lancip dan ramping.",
+    sumber:
+      "https://asset.kompas.com/crops/SPk9kpKap3E_8Zc2HGy-uZk-w5k=/126x47:874x420/750x500/data/photo/2021/05/18/60a393487799f.png",
+  },
+  {
+    namaSenjata: "Golok Ciomas",
+    asal: "Banten",
+    foto: GolokCiomas,
+    deskripsi:
+      "Golok Ciomas, senjata tradisional asal Banten. Dibuat dengan ritual dan persyaratan khusus. Selain golok ciomas, Banten juga memiliki golok sulangka, parang, dan congkrang sebagai senjata tradisional.",
+    sumber:
+      "https://faktabanten.co.id/wp-content/uploads/2017/04/received_1365859740118866.jpeg",
+  },
+  {
+    namaSenjata: "Tombak Trisula",
+    asal: "Sumatera Selatan",
+    foto: TombakTrisula,
+    deskripsi:
+      "Tombak Trisula, senjata Sumatera Selatan, memiliki tiga mata tombak yang menyolok, berbentuk tombak kayu sepanjang 180 cm. Selain tombak trisula, Sumatera Selatan juga memiliki senjata seperti keris Palembang dan klewang hembrug.",
+    sumber:
+      "https://www.senibudayabetawi.com/wp-content/uploads/2021/09/DSC00188.jpg",
+  },
+  {
+    namaSenjata: "Piso Gaja Dompak",
+    asal: "Sumatera Utara",
+    foto: PisoGajaDompak,
+    deskripsi:
+      "Piso Gaja Dompak, senjata tradisional Sumatera Utara, dulunya sering digunakan oleh Raja Batak. Diambil dari nama piso yang artinya pisau, gaja yang artinya gajah, dan dompak yang artinya kewibawaan.",
+    sumber:
+      "https://pariwisataindonesia.id/wp-content/uploads/2021/09/Piso-Gaja-Dompak-e1599753782192.jpg",
+  },
+  {
+    namaSenjata: "Keris Bengkulu",
+    asal: "Bengkulu",
+    foto: KerisBengkulu,
+    deskripsi:
+      "Keris Bengkulu, senjata tradisional dari Bengkulu, memiliki bilah yang lebih lurus, gagang yang elegan, dan hulu yang dihiasi dengan ukiran indah. Sering digunakan dalam upacara adat dan dianggap sebagai benda pusaka.",
+    sumber:
+      "https://asset.kompas.com/crops/MzswZJCyPWTaiDZZTGdzGDBN6u4=/170x115:840x562/750x500/data/photo/2021/05/18/60a37e1eec263.png",
+  },
+];
