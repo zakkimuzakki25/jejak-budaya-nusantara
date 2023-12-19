49167c506928af0cb538bc716bf3b6bebd2af793
ernuyoga <ernuyoganirwana@gmail.com>
2023-12-19 15:20:23 +0700

    content makanan

--- src/components/cards/content/ContentCard.jsx
+++ src/components/cards/content/ContentCard.jsx
@@ -1,15 +1,15 @@
 import './ContentCard.css';
 
-function ContentCard({ namaMakanan, daerah, foto, deskripsi }) {
+function ContentCard({ jenis, nama, daerah, foto, deskripsi, sumber }) {
   return (
     <div className="container-content">
       <div>
-        <img src={foto} alt={namaMakanan} className="foto-content" />
+        <img src={foto} alt={nama} className="image-content" />
+        <h4 className='source-image'>{sumber}</h4>
       </div>
       <div>
-        <h2>{namaMakanan}</h2>
-        <p>{daerah}</p>
-        <p>{deskripsi}</p>
+        <h2 className='title-content'> {jenis} daerah {daerah}: {nama}</h2>
+        <p className='desc-content'>{deskripsi}</p>
       </div>
     </div>
   );
