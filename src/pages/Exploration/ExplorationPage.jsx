c21fb71942e59c27100b8cc69cb6261ecda70983
Rino <rinooktavianridwan@gmail.com>
2023-12-20 06:14:16 +0700

    pp

--- src/pages/Exploration/ExplorationPage.jsx
+++ src/pages/Exploration/ExplorationPage.jsx
@@ -5,20 +5,21 @@ import VectorArrow from "../../assets/support/VectorArrow.svg";
 import "./ExplorationPage.css";
 
 const ExplorationPage = () => {
-<<<<<<< HEAD
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-=======
-    let listWindow = window.localStorage.getItem("listExplorationJejakNusantara");
-    listWindow = listWindow ? listWindow.split("#") : [];
 
-    const clickHandler = (data) => {
-        window.localStorage.setItem("listExplorationJejakNusantara", [...listWindow, data].join("#"));
-        console.log(listWindow);
-    };
+  let listWindow = window.localStorage.getItem("listExplorationJejakNusantara");
+  listWindow = listWindow ? listWindow.split("#") : [];
 
->>>>>>> 5c309a9a0dedddc63e30c8549e40884ef07dab61
+  const clickHandler = (data) => {
+    window.localStorage.setItem(
+      "listExplorationJejakNusantara",
+      [...listWindow, data].join("#")
+    );
+    console.log(listWindow);
+  };
+
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -35,7 +36,13 @@ const ExplorationPage = () => {
         {explorationList.map((exploration, index) => (
           <div
             key={index}
-            className={`card-nusantara-letter ${listWindow.includes(exploration.id) ? (index%2 == 0 ? "active-green" : "active-yellow") : ""}`}
+            className={`card-nusantara-letter ${
+              listWindow.includes(exploration.id)
+                ? index % 2 == 0
+                  ? "active-green"
+                  : "active-yellow"
+                : ""
+            }`}
             onClick={() => scrollToSection(exploration.id)}
           >
             {exploration.letter}
@@ -62,9 +69,17 @@ const ExplorationPage = () => {
                   Sed tristique dui eros, in lacinia tellus pulvinar in.
                   Maecenas suscipit ut ligula at pretium.
                 </div>
-                <Link onClick={() => clickHandler(exploration.id)} to={exploration.path} className="link-path">
-                  <p style={{margin: "0px"}}>lihat selengkapnya</p>
-                  <img src={VectorArrow} alt="Vector Arrow" style={{height: "fit-content"}}/>
+                <Link
+                  onClick={() => clickHandler(exploration.id)}
+                  to={exploration.path}
+                  className="link-path"
+                >
+                  <p style={{ margin: "0px" }}>lihat selengkapnya</p>
+                  <img
+                    src={VectorArrow}
+                    alt="Vector Arrow"
+                    style={{ height: "fit-content" }}
+                  />
                 </Link>
               </div>
             </>
@@ -88,9 +103,17 @@ const ExplorationPage = () => {
                   Sed tristique dui eros, in lacinia tellus pulvinar in.
                   Maecenas suscipit ut ligula at pretium.
                 </div>
-                <Link onClick={() => clickHandler(exploration.id)} to={exploration.path} className="link-path">
-                  <p style={{margin: "0px"}}>lihat selengkapnya</p>
-                  <img src={VectorArrow} alt="Vector Arrow" style={{height: "fit-content"}}/>
+                <Link
+                  onClick={() => clickHandler(exploration.id)}
+                  to={exploration.path}
+                  className="link-path"
+                >
+                  <p style={{ margin: "0px" }}>lihat selengkapnya</p>
+                  <img
+                    src={VectorArrow}
+                    alt="Vector Arrow"
+                    style={{ height: "fit-content" }}
+                  />
                 </Link>
               </div>
 
