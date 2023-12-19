8982664697f9543a1e55308b9e0b37974e180a48
Rino <rinooktavianridwan@gmail.com>
2023-12-19 18:33:41 +0700

    datarumah

---
+++ src/data/rumah.jsx
@@ -0,0 +1,53 @@
+import KrongBade from "../assets/image/Krong-Bade.svg";
+import Bolon from "../assets/image/Bolon.svg";
+import Gadang from "../assets/image/Gadang.svg";
+import SelasoJatuhKembar from "../assets/image/Selaso-Jatuh-Kembar.svg";
+import RumahAtapLimasPotong from "../assets/image/Rumah-Atap-Limas-Potong.svg";
+
+export const listRumahAdat = [
+  {
+    namaRumahAdat: "Krong Bade",
+    daerah: "Aceh",
+    foto: KrongBade,
+    deskripsi:
+      "Rumah Krong Bade Aceh berbentuk memanjang dari timur ke barat menyerupai persegi panjang. Di bagian depan rumah dilengkapi dengan tangga untuk masuk ke dalam rumah. Umumnya, tangga pada rumah adat Aceh ini jumlahnya ganjil, yaitu sekitar 7 hingga 9 anak tangga.",
+    sumber:
+      "https://www.viva.co.id/foto/1532117-6-fakta-rumah-tradisonal-krong-bade-aceh-rumah-tanpa-paku",
+  },
+  {
+    namaRumahAdat: "Bolon",
+    daerah: "Sumatera Utara",
+    foto: Bolon,
+    deskripsi:
+      "Rumah adat Bolon Sumatera Utara terdiri dari dua bagian, yaitu Jabu Bolon dan Jabu Parsakitan. Jabu Bolon biasa menjadi tempat untuk keluarga besar, sedangkan Jabu Parsakitan adalah tempat untuk membicarakan masalah adat. Keunikan dari rumah adat Sumatera Utara ini adalah tidak ada sekatan antara setiap ruangan, sehingga semua anggota keluarga tidur bersama di dalam ruangan besar.",
+    sumber:
+      "https://blog-static.mamikos.com/wp-content/uploads/2023/01/Keunikan-Rumah-Adat-Bolon-Sumatera-Utara-beserta-Ciri-ciri-dan-Makna.png.webp",
+  },
+  {
+    namaRumahAdat: "Gadang",
+    daerah: "Sumatera Barat",
+    foto: Gadang,
+    deskripsi:
+      "Rumah adat Gadang dari Sumatera Barat terlihat mewah dengan beberapa atap yang runcing dan menjulang ke atas. Rumah adat Gadang terbuat dari ijuk dan bentuknya mirip seperti tanduk kerbau, yang melambangkan kemenangan suku Minang dalam perlombaan adu kerbau di Jawa.",
+    sumber:
+      "https://www.ruparupa.com/blog/wp-content/uploads/2022/02/rumah-gadang-minangkabau.jpeg",
+  },
+  {
+    namaRumahAdat: "Selaso Jatuh Kembar",
+    daerah: "Riau",
+    foto: SelasoJatuhKembar,
+    deskripsi:
+      "Rumah Selaso Jatuh Kembar di Riau memiliki arti rumah dengan dua selasar. Masyarakat Riau tidak menjadikan rumah ini sebagai tempat tinggal, tetapi hanya menggunakannya untuk acara adat.",
+    sumber:
+      "https://asset.kompas.com/crops/KuUM76gVQ5NUZA_Qbc5V8sV9biY=/12x0:603x394/750x500/data/photo/2020/10/05/5f7a7b80e8bdb.jpg",
+  },
+  {
+    namaRumahAdat: "Rumah Atap Limas Potong",
+    daerah: "Kepulauan Riau",
+    foto: RumahAtapLimasPotong,
+    deskripsi:
+      "Rumah adat Limas Potong dari Kepulauan Riau terlihat sangat sederhana. Berbentuk seperti rumah panggung, yang memanjang ke belakang dengan dinding kayu tersusun secara vertikal. Atap dari rumah adat Limas Potong memiliki lima bumbungan dengan menggunakan seng berwarna merah.",
+    sumber:
+      "https://assets.kompasiana.com/items/album/2020/10/25/rumah-melayu-atap-limas-5f954f6dd541df729562e083.png?t=o&v=740&x=416",
+  },
+];
