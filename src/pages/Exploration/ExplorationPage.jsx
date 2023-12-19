5c309a9a0dedddc63e30c8549e40884ef07dab61
zakki <zakkimuzakki050@gmail.com>
2023-12-19 23:35:45 +0700

    tanda check list eksplorasi done

--- src/pages/Exploration/ExplorationPage.jsx
+++ src/pages/Exploration/ExplorationPage.jsx
@@ -4,6 +4,14 @@ import VectorArrow from "../../assets/support/VectorArrow.svg";
 import "./ExplorationPage.css";
 
 const ExplorationPage = () => {
+    let listWindow = window.localStorage.getItem("listExplorationJejakNusantara");
+    listWindow = listWindow ? listWindow.split("#") : [];
+
+    const clickHandler = (data) => {
+        window.localStorage.setItem("listExplorationJejakNusantara", [...listWindow, data].join("#"));
+        console.log(listWindow);
+    };
+
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -13,13 +21,14 @@ const ExplorationPage = () => {
       });
     }
   };
+
   return (
     <div id="exploration-container">
       <div className="nusantara-container">
         {explorationList.map((exploration, index) => (
           <div
             key={index}
-            className="card-nusantara-letter"
+            className={`card-nusantara-letter ${listWindow.includes(exploration.id) ? (index%2 == 0 ? "active-green" : "active-yellow") : ""}`}
             onClick={() => scrollToSection(exploration.id)}
           >
             {exploration.letter}
@@ -46,7 +55,7 @@ const ExplorationPage = () => {
                   Sed tristique dui eros, in lacinia tellus pulvinar in.
                   Maecenas suscipit ut ligula at pretium.
                 </div>
-                <Link to={exploration.path} className="link-path">
+                <Link onClick={() => clickHandler(exploration.id)} to={exploration.path} className="link-path">
                   <p style={{margin: "0px"}}>lihat selengkapnya</p>
                   <img src={VectorArrow} alt="Vector Arrow" style={{height: "fit-content"}}/>
                 </Link>
@@ -72,7 +81,7 @@ const ExplorationPage = () => {
                   Sed tristique dui eros, in lacinia tellus pulvinar in.
                   Maecenas suscipit ut ligula at pretium.
                 </div>
-                <Link to={exploration.path} className="link-path">
+                <Link onClick={() => clickHandler(exploration.id)} to={exploration.path} className="link-path">
                   <p style={{margin: "0px"}}>lihat selengkapnya</p>
                   <img src={VectorArrow} alt="Vector Arrow" style={{height: "fit-content"}}/>
                 </Link>
