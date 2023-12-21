82251df7ca470ec54a946770d8b5aa89d0fad7fc
Rino <rinooktavianridwan@gmail.com>
2023-12-21 12:49:02 +0700

    awanhome

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import maskot from "/src/assets/maskots/MaskotMain.svg";
 import maskotAwan from "/src/assets/maskots/MaskotMainSmileWithCloud.svg";
 import ExploreButton from "../../components/button/ExploreButton";
 import HomeSlider from "../../components/slider/HomeSlider";
-import HomeAwan from "../../components/background/HomeAwan";
 import VectorArrow from "../../assets/support/VectorArrow.svg";
 import { faktaMenarik } from "../../data/faktaMenarik";
 import { Link } from "react-router-dom";
@@ -12,12 +11,17 @@ import { useEffect, useState } from "react";
 import BubbleChat from "../../components/bubbleChat/BubbleChat";
 import Navbar from "../../components/layout/Navbar";
 import Footer from "../../components/layout/Footer";
+import BatikAwan from "../../assets/support/BatikAwan.svg";
 
 function Home() {
   const token = window.localStorage.getItem("tokenJBN");
   const date = new Date(Date.now());
-  const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-  const [isLoaded, setIsLoaded] = useState(token != formattedDate ? true : false);
+  const formattedDate = `${date.getDate()}-${
+    date.getMonth() + 1
+  }-${date.getFullYear()}`;
+  const [isLoaded, setIsLoaded] = useState(
+    token != formattedDate ? true : false
+  );
   const [isLoaded2, setIsLoaded2] = useState(false);
 
   useEffect(() => {
@@ -32,13 +36,13 @@ function Home() {
       window.scrollTo({
         top: 0,
         left: 0,
-        behavior: 'smooth'
+        behavior: "smooth",
       });
     }
   }, [isLoaded2]);
 
   return (
-    <div style={{overflow: "hidden"}}>
+    <div style={{ overflow: "hidden" }}>
       <Navbar />
       <div className="home">
         <div className="home-explore">
@@ -48,8 +52,8 @@ function Home() {
           <div className="home-intro">
             <h1>Selamat Datang di JBN</h1>
             <p>
-              Kami mengajak Anda untuk menemukan dan memahami keindahan
-              warisan budaya, seni, dan tradisi yang telah menjadi bagian tak
+              Kami mengajak Anda untuk menemukan dan memahami keindahan warisan
+              budaya, seni, dan tradisi yang telah menjadi bagian tak
               terpisahkan dari kehidupan masyarakat Indonesia.
             </p>
           </div>
@@ -62,11 +66,40 @@ function Home() {
         </div>
       </div>
       <div className="home-awan">
-        <HomeAwan />
         <div className="home-content">
+          <img
+            src={BatikAwan}
+            alt="batik-atas"
+            className="batik-awan-kiriatas"
+          />
+          <img
+            src={BatikAwan}
+            alt="batik-atas"
+            className="batik-awan-kananatas"
+          />
+          <img
+            src={BatikAwan}
+            alt="batik-bawah"
+            className="batik-awan-kiribawah-atas"
+          />
+          <img
+            src={BatikAwan}
+            alt="batik-bawah"
+            className="batik-awan-kiribawah-bawah"
+          />
+          <img
+            src={BatikAwan}
+            alt="batik-bawah"
+            className="batik-awan-kananbawah-atas"
+          />
+          <img
+            src={BatikAwan}
+            alt="batik-bawah"
+            className="batik-awan-kananbawah-bawah"
+          />
           <div className="home-interesting-fact">
             <h1>Fakta Menarik</h1>
-            <Link to={"/fakta-menarik"}className="link-path">
+            <Link to={"/fakta-menarik"} className="link-path">
               <p style={{ margin: "0px" }}>lihat semua</p>
               <img src={VectorArrow} style={{ height: "fit-content" }} />
             </Link>
@@ -92,7 +125,9 @@ function Home() {
       ) : isLoaded2 ? (
         <>
           <BubbleChat
-            message={"Dengan mengklik bagian ini, kamu dapat memulai perjalanan virtual kamu untuk menjelajahi keberagaman budaya Nusantara. Temukan cerita-cerita menarik, warisan seni yang unik, dan tradisi-tradisi yang memperkaya kehidupan masyarakat Indonesia.Bagian ini digunakan untuk eksplorasi"}
+            message={
+              "Dengan mengklik bagian ini, kamu dapat memulai perjalanan virtual kamu untuk menjelajahi keberagaman budaya Nusantara. Temukan cerita-cerita menarik, warisan seni yang unik, dan tradisi-tradisi yang memperkaya kehidupan masyarakat Indonesia.Bagian ini digunakan untuk eksplorasi"
+            }
             setBubbleChat={setIsLoaded2}
             zIndex={97}
           />
