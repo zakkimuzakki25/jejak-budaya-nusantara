6c1a0728782895fdc26e5030b09805118b65699c
Rino <rinooktavianridwan@gmail.com>
2023-12-21 14:38:49 +0700

    navbarhp

--- src/components/layout/NavbarPopUp.jsx
+++ src/components/layout/NavbarPopUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NavbarPopUp.css";
 import { useNavigate } from "react-router-dom";
 
@@ -6,8 +6,34 @@ import ExploreButton from "../button/ExploreButton";
 
 const NavbarPopUp = ({ isVisible, items, togglePopUp }) => {
   const nav = useNavigate();
-  const firstSection = items.slice(0, 5);
-  const secondSection = items.slice(5);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [firstSection, setFirstSection] = useState([]);
+  const [secondSection, setSecondSection] = useState([]);
+  const [thirdSection, setThirdSection] = useState([]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowWidth <= 600) {
+      setFirstSection(items.slice(0, 3));
+      setSecondSection(items.slice(3, 6));
+      setThirdSection(items.slice(6));
+    } else {
+      setFirstSection(items.slice(0, 5));
+      setSecondSection(items.slice(5));
+      setThirdSection([]);
+    }
+  }, [items, windowWidth]);
 
   const handleNavClick = (path) => {
     nav(path);
@@ -41,6 +67,20 @@ const NavbarPopUp = ({ isVisible, items, togglePopUp }) => {
             </div>
           ))}
         </div>
+        {thirdSection.length > 0 && (
+          <div className="popup-section">
+            {thirdSection.map((item, index) => (
+              <div key={index} className="slider-item">
+                <button
+                  onClick={() => handleNavClick(item.path)}
+                  className="slider-link"
+                >
+                  {item.nama}
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <div className="button-explore-section-navbar" onClick={() => togglePopUp(false)}>
         <ExploreButton data={"EKSPLORASI"}/>
