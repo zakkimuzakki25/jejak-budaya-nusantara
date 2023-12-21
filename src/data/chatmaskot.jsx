b6a3cd2fde67ca925507abcff7ca1c992156f632
Rino <rinooktavianridwan@gmail.com>
2023-12-21 19:32:26 +0700

    pushchat

---
+++ src/data/chatmaskot.jsx
@@ -0,0 +1,16 @@
+// Mendefinisikan daftar chat maskot
+export const listchatMaskot = [
+    "Hai, bagaimana kabarmu hari ini?",
+    "Hmmm, apakah kamu sudah menemukan atau mempelajari hal baru hari ini?",
+    "Apa kegiatan favoritmu di akhir pekan?",
+    "Tahu tidak, banyak cerita menarik yang tersembunyi di sekitarmu!",
+    "Sedang apa? Aku penasaran!",
+    "Ada yang bisa kubantu hari ini?",
+    "Tahukah kamu bahwa setiap daerah di Indonesia memiliki keunikan sendiri?",
+    "Ayo berbagi cerita tentang pengalaman terbaikmu di Indonesia!",
+    "Jika ada satu tempat di Indonesia yang ingin kamu kunjungi, itu akan menjadi apa?",
+    "Bagikan resep makanan favoritmu dari Indonesia, siapa tahu aku bisa mencobanya juga!",
+    "Apa hal paling menarik yang pernah kamu pelajari tentang budaya Indonesia?",
+    "Pernahkah kamu menghadiri festival budaya di Indonesia?"
+  ];
+  
