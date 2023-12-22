d637a202b3c13598480c13d50118f78a360f1cc9
zakki <zakkimuzakki050@gmail.com>
2023-12-22 08:59:06 +0700

    lagu daerah update data

--- src/data/lagu.jsx
+++ src/data/lagu.jsx
@@ -19,7 +19,7 @@ export const listLagu = [
     namaLagu: "Bungong Jempa",
     daerah: "Aceh",
     foto: BungongJempa,
-    deskripsi: "Bungong Jempa adalah lagu daerah yang berasal dari Aceh. Lagu ini menceritakan tentang bunga cempaka yang sangat indah di Provinsi Aceh.",
+    deskripsi: "Bungong Jempa adalah harmoni indah yang merayakan kecantikan bunga cempaka, melambangkan keelokan Provinsi Aceh. Melalui melodi yang memikat, lagu ini mengajak pendengar untuk merenungkan keindahan alam Aceh.",
     sumber: "https://imgx.sonora.id/crop/0x0:0x0/700x465/photo/2020/03/26/4005935201.jpg"
   },
   {
@@ -40,7 +40,7 @@ export const listLagu = [
     namaLagu: "Soleram",
     daerah: "Riau",
     foto: Soleram,
-    deskripsi: "Soleram adalah lagu daerah yang berasal dari Riau. Lagu ini berisi nasihat terkait adat istiadat dan agama agar selalu terjaga dengan baik.",
+    deskripsi: "Soleram, lagu daerah Riau, menjadi corong nasihat terhadap pentingnya menjaga tradisi dan agama dalam kehidupan sehari-hari. Liriknya menyampaikan pesan kearifan lokal yang turun-temurun.",
     sumber: "https://www.riauonline.co.id/foto/bank/images2/Soleram.jpg"
   },
   {
@@ -54,70 +54,70 @@ export const listLagu = [
     namaLagu: "O Ina Ni Keke",
     daerah: "Sulawesi Utara",
     foto: InaNiKeke,
-    deskripsi: "Ina Ni Keke adalah lagu daerah dari Sulawesi Utara. Lagu ini menggambarkan kecantikan alam dan budaya daerah tersebut.",
+    deskripsi: "O Ina Ni Keke mengekspresikan keelokan alam dan kekayaan budaya Sulawesi Utara. Lagu ini adalah himbauan untuk menyayangi dan menjaga alam serta tradisi setempat.",
     sumber: "https://www.materibelajar.id/2017/12/makna-lagu-o-ina-ni-keke.html"
   },
   {
     namaLagu: "Yamko Rambe Yamko",
     daerah: "Papua",
     foto: YamkoRambeYamko,
-    deskripsi: "Yamko Rambe Yamko adalah lagu tradisional Papua yang merujuk pada kegembiraan dan semangat orang Papua.",
+    deskripsi: "Merujuk pada semangat dan kebahagiaan masyarakat Papua, lagu tradisional ini menjadi cerminan kegembiraan yang diwariskan dari generasi ke generasi.",
     sumber: "https://medan.tribunnews.com/2022/12/06/materi-belajar-lirik-terjemahan-laguyamko-rambe-yamko-makna-dan-penciptaannya"
   },
   {
     namaLagu: "Rasa Sayange",
     daerah: "Maluku",
     foto: RasaSayange,
-    deskripsi: "Rasa Sayange adalah lagu rakyat Maluku yang menggambarkan kasih sayang antarmanusia.",
+    deskripsi: "Rasa Sayange adalah lagu rakyat Maluku yang menggambarkan kasih sayang antar manusia. Sebagai lagu rakyat Maluku, Rasa Sayange mengangkat tema kasih sayang antarmanusia. Dalam melodi yang indah, pesan kebaikan dan cinta tersirat dengan manis.",
     sumber: "https://www.krjogja.com/musik/1242457238/lirik-lagu-rasa-sayange-ciptaan-paulus-pea"
   },
   {
     namaLagu: "Ampar Ampar Pisang",
     daerah: "Kalimantan Selatan",
     foto: AmparAmparPisang,
-    deskripsi: "Ampar Ampar Pisang adalah lagu daerah yang populer di Kalimantan Selatan. Lagu ini menceritakan tentang seseorang yang kehilangan pisangnya.",
+    deskripsi: "Ampar Ampar Pisang adalah lagu daerah yang populer di Kalimantan Selatan. Lagu ini menceritakan tentang seseorang yang kehilangan pisangnya. Lagu yang populer di Kalimantan Selatan ini bercerita tentang seseorang yang kehilangan pisangnya, namun dalam liriknya tersemat makna yang lebih dalam tentang kehilangan dan harapan.",
     sumber: "https://www.sonora.id/read/422077666/liri-lagu-daerah-ampar-ampar-pisang-dari-kalimantan-selatan"
   },
   {
     namaLagu: "Apuse",
     daerah: "Sulawesi Tengah",
     foto: Apuse,
-    deskripsi: "Apuse adalah lagu daerah dari Sulawesi Tengah. Lagu ini bercerita tentang keindahan alam dan kehidupan sehari-hari di daerah tersebut.",
+    deskripsi: "Apuse dari Sulawesi Tengah adalah kisah tentang keindahan alam serta kehidupan sehari-hari di daerah tersebut. Melalui nada yang mengalun, lagu ini membawa pendengar ke dalam suasana yang damai.",
     sumber: "https://www.suarakarya.id/muda/pr-2602946471/lirik-lagu-apuse-dan-terjemahannya"
   },
   {
     namaLagu: "Manuk Dadali",
     daerah: "Jawa Barat",
     foto: ManukDadali,
-    deskripsi: "Manuk Dadali adalah lagu daerah yang berasal dari Jawa Barat. Lagu ini menggambarkan kehidupan masyarakat petani di daerah tersebut.",
+    deskripsi: "Manuk Dadali adalah lagu daerah yang berasal dari Jawa Barat. Lagu ini menggambarkan kehidupan masyarakat petani di daerah tersebut. Merupakan cerminan kehidupan masyarakat petani di Jawa Barat, lagu ini menggambarkan perjuangan dan keindahan sederhana dalam kehidupan sehari-hari.",
     sumber: "https://id.pinterest.com/pin/570760952789524692/"
   },
   {
     namaLagu: "Tokecang",
     daerah: "Jawa Barat",
     foto: Tokecang,
-    deskripsi: "Tokecang adalah lagu daerah Jawa Barat yang menggambarkan tentang alat musik tradisional dan kegiatan sehari-hari masyarakat.",
+    deskripsi: "Tokecang adalah lagu daerah Jawa Barat yang menggambarkan tentang alat musik tradisional dan kegiatan sehari-hari masyarakat. Lagu ini menghadirkan nuansa tentang alat musik tradisional dan kegiatan sehari-hari masyarakat dengan lirik yang menggugah semangat.",
     sumber: "https://id.pinterest.com/pin/810929476630009827/"
   },
   {
     namaLagu: "Jali Jali",
     daerah: "Kalimantan Timur",
     foto: JaliJali,
-    deskripsi: "Jali Jali adalah lagu daerah yang berasal dari Kalimantan Timur. Lagu ini populer sebagai lagu anak-anak.",
+    deskripsi: "Jali Jali adalah lagu daerah yang berasal dari Kalimantan Timur. Lagu ini populer sebagai lagu anak-anak. Sebagai lagu anak-anak dari Kalimantan Timur, Jali Jali menyajikan cerita yang menghibur dengan pesan yang sederhana namun dalam.",
     sumber: "https://palembang.tribunnews.com/2023/01/26/chord-lagu-jali-jali-lagu-daerah-jakarta-lengkap-lirik-bahasa-betawi-makna-sebagai-penghibur-hati?page=all"
   },
   {
     namaLagu: "Poco Poco",
     daerah: "Sulawesi Utara",
     foto: PocoPoco,
-    deskripsi: "Poco Poco adalah lagu daerah yang berasal dari Sulawesi Utara. Lagu ini memiliki irama yang energik dan menjadi tarian yang populer di Indonesia.",
+    deskripsi: "Poco Poco adalah lagu daerah yang berasal dari Sulawesi Utara. Lagu ini memiliki irama yang energik dan menjadi tarian yang populer di Indonesia. Lagu energik dari Sulawesi Utara ini menjadi tarian yang populer di Indonesia. Iramanya yang dinamis memicu semangat dan keceriaan bagi para penari.",
     sumber: "https://www.youtube.com/watch?v=U2shB399qes"
   },
   {
     namaLagu: "Cublak Cublak Suweng",
     daerah: "Jawa Tengah",
     foto: CublakCublakSuweng,
-    deskripsi: "Cublak Cublak Suweng adalah lagu daerah yang berasal dari Jawa Tengah. Lagu ini populer sebagai lagu anak-anak.",
+    deskripsi: "Cublak Cublak Suweng adalah lagu daerah yang berasal dari Jawa Tengah. Populer sebagai lagu anak-anak dari Jawa Tengah, lagu ini membawa nuansa ceria dengan melodi yang mengundang untuk bernyanyi dan bermain.",
     sumber: "https://www.kelaspintar.id/blog/tips-pintar/inilah-tangga-nada-lagu-cublak-cublak-suweng-lagu-daerah-yang-bisa-kamu-pelajari-10668"
   },
 ];
