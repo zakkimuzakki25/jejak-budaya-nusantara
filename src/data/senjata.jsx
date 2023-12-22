feae14c68350a7416c6cce8a5165243a1d1a74d0
Rino <rinooktavianridwan@gmail.com>
2023-12-22 08:35:02 +0700

    dataSenjata

--- src/data/senjata.jsx
+++ src/data/senjata.jsx
@@ -3,6 +3,16 @@ import GolokCiomas from "../assets/image/Golok-Ciomas.svg";
 import TombakTrisula from "../assets/image/Tombak-Trisula.svg";
 import PisoGajaDompak from "../assets/image/Piso-Gaja-Dompak.svg";
 import KerisBengkulu from "../assets/image/Keris-Bengkulu.svg";
+import senjataBadikTumbuk from "../assets/image/senjata-Badik-Tumbuk.svg";
+import senjataCelurit from "../assets/image/senjata-Celurit.svg";
+import senjataGolok from "../assets/image/senjata-Golok.svg";
+import senjataKandik from "../assets/image/senjata-Kandik.svg";
+import senjataKeris from "../assets/image/senjata-Keris.svg";
+import senjataKujang from "../assets/image/senjata-Kujang.svg";
+import senjataRencong from "../assets/image/senjata-Rencong.svg";
+import senjataSiwarPanjang from "../assets/image/senjata-Siwar-Panjang.svg";
+import senjataTerapang from "../assets/image/senjata-Terapang.svg";
+import senjataTulup from "../assets/image/senjata-Tulup.svg";
 
 export const listSenjata = [
   {
@@ -49,5 +59,95 @@ export const listSenjata = [
       "Keris Bengkulu, senjata tradisional dari Bengkulu, memiliki bilah yang lebih lurus, gagang yang elegan, dan hulu yang dihiasi dengan ukiran indah. Sering digunakan dalam upacara adat dan dianggap sebagai benda pusaka.",
     sumber:
       "https://asset.kompas.com/crops/MzswZJCyPWTaiDZZTGdzGDBN6u4=/170x115:840x562/750x500/data/photo/2021/05/18/60a37e1eec263.png",
+  },
+  {
+    namaSenjata: "Rencong",
+    daerah: "Aceh",
+    foto: senjataRencong,
+    deskripsi:
+      "Rencong, senjata tradisional pertama Indonesia yang berasal dari Aceh, memiliki bentuk yang unik dengan bahan pembuatan utama dari kuningan atau besi putih, sementara sarungnya biasanya terbuat dari tanduk kerbau. Meskipun ada yang membuat sarung rencong dari kayu sekarang ini, senjata ini tetap menjadi simbol identitas diri, keberanian, dan ketangguhan masyarakat Aceh.",
+    sumber:
+      "https://upload.wikimedia.org/wikipedia/commons/3/3f/COLLECTIE_TROPENMUSEUM_Dolk_met_rechthoekig_gebogen_hoornen_greep_en_houten_schede_TMnr_151-19.jpg",
+  },
+  {
+    namaSenjata: "Badik Tumbuk",
+    daerah: "Kepulauan Riau",
+    foto: senjataBadikTumbuk,
+    deskripsi:
+      "Badik Tumbuk Lado, senjata tradisional Kepulauan Riau, merupakan alat yang digunakan untuk membela diri dari musuh. Senjata ini terbuat dari campuran besi dan baja dengan panjang sekitar 20-30 cm. Bagian sarung penutupnya dibuat dari kayu lunak yang dilapisi dengan lempengan emas atau perak. Dahulu kala, Badik Tumbuk Lado sering dipergunakan sebagai alat pertahanan diri oleh penduduk setempat. ",
+    sumber:
+      "https://asset.kompas.com/crops/UembnlntLOwWElCEEsme3UsXIOs=/0x55:796x586/750x500/data/photo/2021/05/18/60a37c00bb15c.png",
+  },
+  {
+    namaSenjata: "Siwar Panjang",
+    daerah: "Kepulauan Bangka Belitung",
+    foto: senjataSiwarPanjang,
+    deskripsi:
+      "Siwar Panjang, senjata khas Kepulauan Bangka Belitung, memiliki ciri khas bilah yang membungkuk ke dalam dan terbuat dari besi tempa, sedangkan tangkainya menggunakan kayu. Selain Siwar Panjang, terdapat pula senjata khas lainnya seperti kedik dan parang badau yang menjadi bagian dari identitas Kepulauan Bangka Belitung.",
+    sumber:
+      "https://asset.kompas.com/crops/OWT2IreD65gxVRY8HTYTdnIlbuI=/129x43:883x420/750x500/data/photo/2021/05/18/60a399e456c46.png",
+  },
+  {
+    namaSenjata: "Terapang",
+    daerah: "Lampung",
+    foto: senjataTerapang,
+    deskripsi:
+      "Terapang, senjata tradisional dari Lampung, memiliki kemiripan dengan keris, Moms.Meskipun menyerupai keris, bilah terapang sedikit berlekuk. Keunikan senjata ini terletak pada adanya ukiran kepala atau burung pada bagian sarungnya, yang melambangkan keberanian.",
+    sumber:
+      "https://radartuba.disway.id/upload/5a38a0a31ef68fc34b14be7f54607786.jpg",
+  },
+  {
+    namaSenjata: "Kujang",
+    daerah: "Jawa Barat",
+    foto: senjataKujang,
+    deskripsi:
+      "Jawa Barat memiliki senjata khas yang disebut kujang. Kujang, senjata tradisional Indonesia ini, memiliki ukuran sekitar 20–25 cm, dan sering dijadikan aksesoris pada pakaian adat laki-laki Jawa Barat. Selain kujang, senjata tradisional lainnya dari Jawa Barat meliputi bedog, baliung, congkrang, arit, sulimat, dan balincong.",
+    sumber:
+      "https://asset.kompas.com/crops/nOL4UNVsVkPeovnmKVoZyywFS_Q=/300x146:667x390/750x500/data/photo/2021/05/17/60a234db2f46e.png",
+  },
+  {
+    namaSenjata: "Golok",
+    daerah: "DKI Jakarta",
+    foto: senjataGolok,
+    deskripsi:
+      "Golok, senjata tradisional Indonesia, menjadi simbol keberanian dan kekuatan. DKI Jakarta juga memiliki senjata khas ini, yang memiliki bilah panjang dan tajam terbuat dari besi atau baja, serta gagang dari kayu keras. Selain golok, senjata tradisional lainnya di DKI Jakarta meliputi badik cangkingan, belati, punta, trisula, toya, dan tusuk konde.",
+    sumber:
+      "https://kebudayaan.kemdikbud.go.id/mkn/wp-content/uploads/sites/61/2017/02/GOLOK.jpg",
+  },
+  {
+    namaSenjata: "Keris",
+    daerah: "Yogyakarta",
+    foto: senjataKeris,
+    deskripsi:
+      "Keris bukan hanya menjadi senjata tradisional dari Jawa Tengah, tetapi juga dari Daerah Istimewa Yogyakarta. Meskipun tidak ada perbedaan signifikan antara keris Jawa Tengah dan DI Yogyakarta, namun terdapat perbedaan pada jumlah ukiran dan lekukannya.",
+    sumber:
+      "https://www.sobatjogja.com/wp-content/uploads/2014/11/Keris-Sebagai-Sebuah-Symbol-Dan-Kekuasaan.jpg.webp",
+  },
+  {
+    namaSenjata: "Celurit",
+    daerah: "Jawa Timur",
+    foto: senjataCelurit,
+    deskripsi:
+      "Celurit, senjata tradisional Indonesia dari Jawa Timur, berasal dari Suku Madura dan menjadi ciri khas masyarakat Jawa Timur. Senjata ini memiliki bentuk melengkung seperti bulan sabit dengan ujung yang sangat tajam dan runcing.",
+    sumber:
+      "https://pariwisataindonesia.id/wp-content/uploads/2020/10/Clurit-foto-by-idntimes.com_.jpg",
+  },
+  {
+    namaSenjata: "Kandik",
+    daerah: "Bali",
+    foto: senjataKandik,
+    deskripsi:
+      "Kandik, senjata tradisional Indonesia yang berasal dari Bali, adalah sejenis kapak yang sering digunakan untuk pekerjaan berat. Selain kandik, terdapat pula senjata tradisional lain di Bali seperti caluktiuk, wedhung, dan keris tayuhan.",
+    sumber:
+      "https://asset.kompas.com/crops/Uf1qCoKJjfNRsHANAiHZjBr2sYM=/179x71:734x442/750x500/data/photo/2021/05/20/60a6454aebb1f.png",
+  },
+  {
+    namaSenjata: "Tulup",
+    daerah: "Nusa Tenggara Barat",
+    foto: senjataTulup,
+    deskripsi:
+      "Nusa Tenggara Barat (NTB) memiliki senjata tradisional Indonesia yang khas bernama tulup, berasal dari suku Sasak. Bentuknya menyerupai sumpitan yang dapat ditiup. Selain tulup, terdapat juga senjata tradisional lain dari NTB, seperti jungkat yang berbentuk hampir dua meter mirip tombak.",
+    sumber:
+      "https://pariwisataindonesia.id/wp-content/uploads/2020/10/senjata-tradisional-tulup-foto-by.jpg",
   },
 ];
