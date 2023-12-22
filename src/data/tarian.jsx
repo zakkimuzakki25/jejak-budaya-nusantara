449bbe06993958a5f95fb0b8a418c66c345580f8
zakki <zakkimuzakki050@gmail.com>
2023-12-22 10:09:28 +0700

    Finally

--- src/data/tarian.jsx
+++ src/data/tarian.jsx
@@ -3,6 +3,15 @@ import TariPendet from "../assets/image/Tari-Pendet.svg";
 import TariTorTor from "../assets/image/Tari-TorTor.svg";
 import TariReog from "../assets/image/Tari-Reog.svg";
 import TariTopeng from "../assets/image/Tari-Topeng.svg";
+import TariSekapurSirih from "../assets/image/Tari-Sekapur-Sirih.svg";
+import TariCokek from "../assets/image/Tari-Cokek.svg";
+import TariYapong from "../assets/image/Tari-Yapong.svg";
+import TariJaipong from "../assets/image/Tari-Jaipong.svg";
+import TariSerimpi from "../assets/image/Tari-Serimpi.svg";
+import TariKecak from "../assets/image/Tari-Kecak.svg";
+import TariCaci from "../assets/image/Tari-Caci.svg";
+import TariGantar from "../assets/image/Tari-Gantar.svg";
+import TariKipas from "../assets/image/Tari-Kipas.svg";
 
 export const listTarian = [
   {
@@ -40,4 +49,67 @@ export const listTarian = [
     deskripsi: "Tari Zapin adalah tarian tradisional Melayu yang berasal dari Riau. Dengan gerakan yang lembut dan melibatkan berbagai ekspresi, Tari Zapin sering diiringi oleh musik tradisional Melayu. Para penari memakai pakaian adat Melayu yang indah dan berwarna. Tarian ini sering dipentaskan dalam berbagai acara kebudayaan dan upacara adat, menciptakan suatu nuansa yang khas dari keindahan budaya Melayu di Indonesia.",
     sumber: "https://bobobox.com/blog/wp-content//uploads/2023/06/Sekilas-tentang-Tari-Topeng-Malangan-1024x651.jpg"
   },
+  {
+    namaTarian: "Tari Sekapur Sirih",
+    daerah: "Jambi",
+    foto: TariSekapurSirih,
+    deskripsi: "Tari Sekapur Sirih, khas dari Jambi, merupakan perwujudan simbol kebersamaan dan persatuan dalam budaya Jambi. Dipentaskan pada acara pernikahan, tarian ini menampilkan keindahan gerakan yang lembut, diiringi dengan melodi lagu-lagu daerah. Para penari membawa sirih yang dihias dengan bunga sebagai simbol kesucian dan keharmonisan dalam pernikahan. Melalui setiap gerakan dan lantunan lagu, tarian ini memancarkan keindahan dan makna mendalam dari kebersamaan yang dijunjung tinggi dalam budaya Jambi.",
+    sumber: "https://www.kabarjambikito.id/budaya-wisata/pr-5914639313/tari-sekapur-sirih-kebanggaan-masyarakat-jambi"
+  },
+  {
+    namaTarian: "Tari Cokek",
+    daerah: "Banten",
+    foto: TariCokek,
+    deskripsi: "Tari Cokek adalah tarian yang berakar dari budaya Betawi, Cina, dan Banten. Gerakannya yang dinamis dan menggoyangkan pinggul menjadi ciri khasnya. Tarian ini mencerminkan kegembiraan dan keceriaan, sering dipentaskan dalam berbagai acara meriah. Keterampilan para penari dalam mengikuti irama musik serta mengolah gerakan tubuh menjadi harmoni visual dan audio yang memukau, menciptakan pengalaman yang menghibur dan meriah bagi penontonnya.",
+    sumber: "https://www.merdeka.com/jatim/mengenal-tari-cokek-asal-betawi-beserta-makna-dan-sejarahnya-baca-lebih-lanjut-kln.html"
+  },
+  {
+    namaTarian: "Tari Yapong",
+    daerah: "DKI Jakarta",
+    foto: TariYapong,
+    deskripsi: "Tari Yapong merupakan bagian tak terpisahkan dari perayaan pernikahan di DKI Jakarta. Para penari membawa kipas dengan gerakan yang lembut, menggambarkan keanggunan dan kemewahan. Dalam setiap gerakannya, tarian ini mencerminkan kehalusan dan keindahan budaya Jakarta yang kaya. Melalui kombinasi gerakan yang lembut dan irama musik yang khas, Tari Yapong memberikan nuansa elegan pada setiap persembahan tarian.",
+    sumber: "https://www.krjogja.com/yogyakarta/1242465926/tari-yapong-massal-di-titik-nol-bakal-meriahkan-ultah-bagong-kussudiardja"
+  },
+  {
+    namaTarian: "Tari Jaipong",
+    daerah: "Jawa Barat",
+    foto: TariJaipong,
+    deskripsi: "Tari Jaipong memadukan keindahan tari tradisional Sunda dengan sentuhan musik modern yang energetik. Gerakan yang dinamis dan penuh semangat menjadi daya tarik utama tarian ini, menjadikannya populer di Indonesia. Dalam setiap langkahnya, tarian ini mengungkapkan kekayaan budaya Jawa Barat yang menggugah hati, sambil membawa suasana penuh kegembiraan dan keceriaan bagi penontonnya.",
+    sumber: "https://www.detik.com/jabar/budaya/d-6214698/sejarah-dan-asal-usul-tari-jaipong-kesenian-jawa-barat-yang-mendunia"
+  },
+  {
+    namaTarian: "Tari Serimpi",
+    daerah: "Jawa Tengah",
+    foto: TariSerimpi,
+    deskripsi: "Tari Serimpi adalah tarian yang umumnya ditampilkan dalam acara kerajaan, menjadi simbol keindahan dan keanggunan. Para penari mengenakan kostum yang indah sambil melantunkan lagu-lagu daerah, memancarkan pesona budaya Jawa Tengah yang kaya akan tradisi dan keelokan.",
+    sumber: "https://budaya.jogjaprov.go.id/berita/detail/1227-tari-serimpi-seni-budaya-sejak-kerajaan-mataram-islam-masa-sultan-agung"
+  },
+  {
+    namaTarian: "Tari Kecak",
+    daerah: "Bali",
+    foto: TariKecak,
+    deskripsi: "Tari Kecak memerlukan keterampilan khusus dalam menari dengan gerakan yang sinkron. Dalam tarian ini, para penari membentuk lingkaran sambil melantunkan lagu-lagu daerah, menciptakan pengalaman visual dan audio yang mengesankan, menampilkan kekuatan kolaborasi dan harmoni dalam keberagaman budaya Bali.",
+    sumber: "https://geti.id/tari-kecak-kesenian-tradisional-kebanggaan-masyarakat-bali/"
+  },
+  {
+    namaTarian: "Tari Caci",
+    daerah: "Nusa Tenggara Timur",
+    foto: TariCaci,
+    deskripsi: "Tari Caci memerlukan keterampilan khusus dalam menari dengan perisai dan cambuk. Gerakan dinamis dan penuh semangat menjadikannya salah satu tarian yang populer di Indonesia. Di balik keberaniannya, tarian ini menggambarkan kekuatan, kejantanan, serta semangat juang suku Nusa Tenggara Timur.",
+    sumber: "https://klasika.kompas.id/baca/mengenal-tari-caci/"
+  },
+  {
+    namaTarian: "Tari Gantar",
+    daerah: "Kalimantan Barat",
+    foto: TariGantar,
+    deskripsi: "Tari Gantar memerlukan keterampilan khusus dalam menari dengan gong dan kempul. Gerakan dinamis dan penuh semangat menjadikannya salah satu tarian yang populer di Indonesia. Tarian ini menggambarkan kegembiraan serta kehidupan masyarakat Kalimantan Barat yang kaya akan keanekaragaman budaya.",
+    sumber: "http://al-mumtaz.ukm.iain-palangkaraya.ac.id/2021/01/mengenal-tarian-suku-dayak-tari-gantar.html"
+  },
+  {
+    namaTarian: "Tari Kipas",
+    daerah: "Maluku",
+    foto: TariKipas,
+    deskripsi: "Tari Kipas memerlukan keterampilan khusus dalam menari dengan kipas. Gerakan lembut dan anggun menjadikannya salah satu tarian yang populer di Indonesia. Tarian ini menggambarkan keelokan alam serta keindahan tradisi budaya Maluku yang kaya.",
+    sumber: "http://kikomunal-indonesia.dgip.go.id/jenis/1/ekspresi-budaya-tradisional/365/tari-kipas-serumpun"
+  }
 ];
