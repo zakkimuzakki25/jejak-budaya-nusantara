a8e21eb9a61ea6d7bd56632e41e52e8636f96c4c
ernuyoga <ernuyoganirwana@gmail.com>
2023-12-22 07:28:19 +0700

    data rumah done

--- src/data/rumah.jsx
+++ src/data/rumah.jsx
@@ -3,6 +3,16 @@ import Bolon from "../assets/image/Bolon.svg";
 import Gadang from "../assets/image/Gadang.svg";
 import SelasoJatuhKembar from "../assets/image/Selaso-Jatuh-Kembar.svg";
 import RumahAtapLimasPotong from "../assets/image/Rumah-Atap-Limas-Potong.svg";
+import BubunganTinggi from "../assets/image/Bubungan-Tinggi.svg";
+import Lamin from "../assets/image/Lamin.svg";
+import BaleLombok from "../assets/image/Bale-Lombok.svg";
+import RumahTongkonan from "../assets/image/Tongkonan.svg";
+import BubunganLima from "../assets/image/Bubungan-Lima.svg";
+import RumahPanjang from "../assets/image/Rumah-Panjang.svg";
+import RumahJoglo from "../assets/image/Rumah-Joglo.svg";
+import RumahHonai from "../assets/image/Rumah-Honai.svg";
+import RumahBaileo from "../assets/image/Rumah-Baileo.svg";
+import RumahBaduy from "../assets/image/Rumah-Baduy.svg";
 
 export const listRumahAdat = [
   {
@@ -49,5 +59,95 @@ export const listRumahAdat = [
       "Rumah adat Limas Potong dari Kepulauan Riau terlihat sangat sederhana. Berbentuk seperti rumah panggung, yang memanjang ke belakang dengan dinding kayu tersusun secara vertikal. Atap dari rumah adat Limas Potong memiliki lima bumbungan dengan menggunakan seng berwarna merah.",
     sumber:
       "https://assets.kompasiana.com/items/album/2020/10/25/rumah-melayu-atap-limas-5f954f6dd541df729562e083.png?t=o&v=740&x=416",
+  },
+  {
+    namaRumahAdat: "Bubungan Tinggi",
+    daerah: "Kalimantan Barat",
+    foto: BubunganTinggi,
+    deskripsi:
+      "Rumah adat Bubungan Tinggi dari Kalimantan Barat memiliki atap yang tinggi dan lancip. Struktur rumah ini biasanya terbuat dari kayu ulin dan bambu. Bubungan Tinggi menjadi simbol keberanian dan ketangguhan masyarakat Dayak. Selain digunakan sebagai tempat tinggal, rumah adat ini juga sering digunakan untuk pertemuan adat dan upacara keagamaan.",
+    sumber:
+      "https://www.indonesia-tourism.com/south-kalimantan/images/bubungan-tinggi-house.jpg",
+  },
+  {
+    namaRumahAdat: "Lamin",
+    daerah: "Sumatera Selatan",
+    foto: Lamin,
+    deskripsi:
+      "Rumah adat Lamin dari Sumatera Selatan memiliki bentuk atap yang melengkung seperti tanduk kerbau. Konstruksi rumah ini terbuat dari kayu ulin dan bambu. Lamin sering digunakan untuk berbagai kegiatan adat, seperti pertemuan masyarakat dan upacara tradisional. Keunikan bentuk atap Lamin menjadi ciri khas yang mudah dikenali.",
+    sumber:
+      "https://asset.kompas.com/crops/49oDBs3LESEoVs8VaIEJtJQ3xtQ=/27x30:527x363/750x500/data/photo/2020/04/29/5ea8da52d1c7f.png",
+  },
+  {
+    namaRumahAdat: "Bale Lombok",
+    daerah: "NTB",
+    foto: BaleLombok,
+    deskripsi:
+      "Bale Lombok adalah rumah adat tradisional dari NTB yang memiliki atap berbentuk seperti perahu terbalik. Rumah ini terbuat dari bahan alam seperti bambu dan jerami. Bale Lombok digunakan sebagai tempat tinggal masyarakat Sasak. Desain atapnya yang unik memberikan kesejukan alami di dalam rumah, cocok dengan iklim tropis di wilayah NTB.",
+    sumber:
+      "https://api2.kemenparekraf.go.id/storage/app/uploads/public/620/b45/94d/620b4594d807d265860293.jpg",
+  },
+  {
+    namaRumahAdat: "Rumah Tongkonan",
+    daerah: "Sulawesi Selatan",
+    foto: RumahTongkonan,
+    deskripsi:
+      "Rumah Tongkonan merupakan rumah adat tradisional suku Toraja di Sulawesi Selatan. Bentuk atapnya yang melengkung seperti perahu terbalik menjadi ciri khas yang unik. Tongkonan sering digunakan sebagai tempat tinggal keluarga dan memiliki makna spiritual yang dalam bagi masyarakat Toraja. Proses pembangunan Tongkonan diwarnai dengan serangkaian upacara adat.",
+    sumber:
+      "https://cdn-u1-gnfi.imgix.net/post/large-rumah-tongkonan-filosofi-dan-simbol-masyarakat-suku-toraja-219436b5ac979aece2074adecc.jpg?fit=crop&crop=faces%2Centropy&lossless=true&auto=compress%2Cformat&w=730&h=486",
+  },
+  {
+    namaRumahAdat: "Bubungan Lima",
+    daerah: "Kalimantan Barat",
+    foto: BubunganLima,
+    deskripsi:
+      "Rumah adat Bubungan Lima dari Kalimantan Barat memiliki atap yang berbentuk lima tingkat. Bubungan Lima merupakan simbol kebersamaan dan solidaritas dalam masyarakat Dayak. Rumah ini juga sering digunakan untuk kegiatan adat dan ritual suku Dayak.",
+    sumber:
+      "https://cdn-cms.pgimgs.com/static/2021/08/1.-Ciri-Khas-Bentuk-Rumah-Adat-Bengkulu.png",
+  },
+  {
+    namaRumahAdat: "Rumah Panjang",
+    daerah: "Kalimantan Barat",
+    foto: RumahPanjang,
+    deskripsi:
+      "Rumah Panjang adalah rumah adat tradisional suku Dayak di Kalimantan Barat. Rumah ini memiliki bentuk panjang dengan atap yang tinggi. Rumah Panjang digunakan sebagai tempat tinggal bersama bagi beberapa keluarga dan sering dihiasi dengan ukiran dan ornamen khas Dayak.",
+    sumber:
+      "https://img.iproperty.com.my/angel/750x1000-fit/wp-content/uploads/sites/5/2022/08/Alt-Text-Arsitektur-Rumah-Panjang.png",
+  },
+  {
+    namaRumahAdat: "Rumah Joglo",
+    daerah: "Jawa Tengah",
+    foto: RumahJoglo,
+    deskripsi:
+      "Rumah Joglo merupakan rumah adat khas Jawa Tengah yang memiliki ciri khas atap tinggi dan melengkung. Struktur rumah ini dibuat tanpa menggunakan paku. Joglo sering digunakan sebagai rumah tinggal atau sebagai pendopo untuk pertemuan besar. Keindahan ukiran pada tiang dan dinding menjadi daya tarik utama rumah ini.",
+    sumber:
+      "https://asset.kompas.com/crops/Q76YxfIWfJwq2ALE61Aoh2o2K14=/0x0:1037x691/750x500/data/photo/2021/09/01/612f052b628f4.jpg",
+  },
+  {
+    namaRumahAdat: "Rumah Honai",
+    daerah: "Papua",
+    foto: RumahHonai,
+    deskripsi:
+      "Rumah Honai, atau yang sering disebut honai, adalah rumah tradisional suku Papua. Honai terbuat dari anyaman daun sagu yang melingkari struktur kayu. Bentuk atap rumah ini menyerupai kerucut dengan pintu masuk yang rendah. Honai memiliki makna spiritual dan biasanya digunakan sebagai tempat pertemuan dan upacara adat suku Papua.",
+    sumber:
+      "https://asset.kompas.com/crops/hfIev8H9bbgU2R7IVxbGR32wOLo=/0x0:1200x800/750x500/data/photo/2021/09/04/61334e074bfbc.jpg",
+  },
+  {
+    namaRumahAdat: "Rumah Baileo",
+    daerah: "Maluku",
+    foto: RumahBaileo,
+    deskripsi:
+      "Rumah Baileo merupakan rumah adat yang berasal dari masyarakat Maluku. Rumah ini memiliki fungsi sebagai kediaman bagi petinggi desa dan juga menjadi tempat berkumpulnya warga sekitar. Bentuknya menyerupai rumah panggung dengan banyak tiang penyangga. Material utama yang digunakan dalam pembuatan rumah Baileo berasal dari alam, seperti kayu, bambu, dan rumbia yang dijadikan sebagai atap. Yang membuatnya sangat istimewa, rumah Baileo dibangun tanpa menggunakan paku, melainkan dengan kait, ijuk, dan pasak kayu.",
+    sumber:
+      "https://asset.kompas.com/crops/sSTDVPrDnbprK5Pq4iItEyRBOyo=/0x61:696x525/750x500/data/photo/2022/02/06/61ff97a64a7d8.jpg",
+  },
+  {
+    namaRumahAdat: "Rumah Baduy",
+    daerah: "Banten",
+    foto: RumahBaduy,
+    deskripsi:
+      "Rumah Baduy adalah rumah adat suku Baduy yang tinggal di daerah Banten. Rumah ini memiliki ciri khas atap berbentuk limas dan dinding yang terbuat dari bambu. Struktur rumah Baduy dibuat tanpa menggunakan paku atau kawat, melainkan dengan menyusun bambu secara rapi dan mengikatnya dengan tali rafia. Hal ini menciptakan tatanan rumah yang kokoh tanpa menggunakan bahan logam.",
+    sumber:
+      "https://o-cdn-cas.sirclocdn.com/parenting/images/rumah-adat-banten_V82juev.width-800.format-webp.webp",
   },
 ];
